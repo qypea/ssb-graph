fc45a8ffc21c95da04265a2d36123b955ae12521
David Winn <q@qypea.com>
2018-08-02 21:40:54 -0700

    Refactor to use createFriendStream to get friends
    Based on unit tests for friends plugin.
    Still no friends returned. Well, just me.

--- index.js
+++ index.js
@@ -3,41 +3,28 @@
 // http://unix.stackexchange.com/questions/65235/universal-node-js-shebang
 // vi: ft=javascript
 
-var ssbKeys = require('ssb-keys')
-var ssbServer = require('scuttlebot')
-  .use(require('scuttlebot/plugins/master'))
+var pull = require('pull-stream');
+var createSbot = require('scuttlebot')
+    .use(require('scuttlebot/plugins/replicate'))
+    .use(require('scuttlebot/plugins/friends'));
 
-var ssbClient = require('ssb-client')
-
-var shsCap = 'XMHDXXFGBJvloCk8fOinzPkKMRqyA2/eH+3VyUr6lig='
-
-var appName = process.env.appName
-var config = require('ssb-config/inject')(appName)
+var config = require('ssb-config/inject')();
 var keys = require('ssb-keys')
-    .loadOrCreateSync(require('path').join(config.path, 'secret'))
+    .loadOrCreateSync(require('path').join(config.path, 'secret'));
 
-var server = ssbServer({
-    port: 45451, timeout: 2001,
-    temp: 'connect',
+var sbot = createSbot({
+    port: 45451,
+    timeout: 2001,
     host: 'localhost',
-    master: keys.id,
+    path: config.path,
     keys: keys,
-    appKey: shsCap
-})
+});
 
-function finish(client) {
-    client.close(true)
-    server.close(true)
-    process.exit(0)
-}
+pull(sbot.friends.createFriendStream(),
+    pull.collect(function (err, info) {
+        if (err) throw err;
 
-ssbClient(keys, { port: 45451, manifest: server.manifest(), caps: { shs: shsCap }}, function (err, client) {
-    if (err) throw err
-
-    client.whoami(function (err, info) {
-        if (err) throw err
-
-        console.log('whoami', info)
-        finish(client)
-    })
-})
+        console.log(info);
+        sbot.close(true);
+        process.exit(0);
+    }));
