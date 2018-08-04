4f51dcb4d2c9a709854a36c46691986282c968a1
David Winn <q@qypea.com>
2018-08-04 07:13:51 -0700

    Print a digraph of the relationships

--- index.js
+++ index.js
@@ -26,22 +26,113 @@ var sbot = createSbot({
 // null: no relationship, false: blocked, true: follows
 var graph = {};
 
-function graphPruneNulls() {
+// Prune the graph structure above of any null/false entries
+function graphPrune() {
     for (start in graph) {
         var row = graph[start];
         for (end in row) {
-            if (row[end] === null) {
+            if (row[end] === null || row[end] === false) {
                 delete row[end];
             }
         }
     }
 }
 
+// Append the next hop from the graph to a set of nodes and a set of relationships
+// @param nodes set of nodes already known
+// @param relationships list of relationships(start, end)
+// @return Same as arguments
+function graphNextHop(nodes, relationships) {
+    var newNodes = [];
+    var newRelationships = [];
+
+    for(var i = 0, size = nodes.length; i < size ; i++){
+        var node = nodes[i];
+
+        var followed = graph[node];
+        if (followed === undefined) {
+            continue;
+        }
+
+        for (follow in followed) {
+            newRelationships.push([node, follow]);
+            newNodes.push(follow);
+        }
+    }
+
+    return [nodes.concat(newNodes), relationships.concat(newRelationships)];
+}
+
+// Return the nodes, relationships within a certain number of hops of a root
+function graphHops(root, hops) {
+    var nodes = [root];
+    var relationships = [];
+
+    for (var i = 0; i < hops; i++) {
+        var ret = graphNextHop(nodes, relationships);
+        nodes = ret[0];
+        relationships = ret[1];
+    }
+    return [nodes, relationships];
+}
+
+var aliases = {};
+var nextAlias = 0;
+
+// Return the alias for a node id
+function alias(node) {
+    if (node in aliases) {
+        return aliases[node];
+    } else {
+        var next = nextAlias++;
+        aliases[node] = next;
+        return next;
+    }
+}
+
+// Return the array of aliases for an array of node ids
+function aliasArray(nodes) {
+    var ret = [];
+
+    for(var i = 0, size = nodes.length; i < size ; i++) {
+        var node = nodes[i];
+        ret.push(alias(node));
+    }
+    return ret;
+}
+
+// Same as aliasArray except an array of arrays
+function aliasArrayArray(nodes) {
+    var ret = [];
+
+    for(var i = 0, size = nodes.length; i < size ; i++) {
+        var node = nodes[i];
+        ret.push(aliasArray(node));
+    }
+    return ret;
+}
+
+// Print a graph in graphviz digraph format
+// Takes in an array of 2-item arrays(source and dest)
+function printGraph(relationships) {
+    console.log('digraph "graph" {');
+    for(var i = 0, size = relationships.length; i < size ; i++) {
+        var rel = relationships[i];
+        console.log(rel[0] + " -> " + rel[1]);
+    }
+    console.log('}');
+}
+
 pull(sbot.friends.stream(),
     pull.drain(function (info) { graph = info; }, function() {
         // Work
-        graphPruneNulls();
-        console.log(graph);
+        graphPrune();
+
+        var ret = graphHops(keys.id, 2);
+        var nodes = ret[0];
+        var relationships = ret[1];
+
+        printGraph(aliasArrayArray(relationships));
 
         // Close out session, exit
         sbot.close(true);
